4bbc72f1aafc05ba7a5e629ef061e6861946170f
Braden Garrison <braden.garrison@duke.edu>
2021-11-03 22:53:41 -0400

    committing server to switch branches

--- sentinel_server.py
+++ sentinel_server.py
@@ -290,9 +290,10 @@ def heart_rate_interval_avg():
     hr_interval = heart_rate_interval(in_data["heart_rate_average_since"],
                                       patient)
     if type(hr_interval) == str:
-        hr_int_avg = "ERROR: no heart rate values in desired time range"
-    hr_int_avg = heart_rate_average(hr_interval)
-    return hr_int_avg, 200
+        return hr_interval, 400
+    else:
+        hr_int_avg = heart_rate_average(hr_interval)
+    return jsonify(hr_int_avg), 200
 
 
 @app.route("/api/patients/<attending_username>", methods=["GET"])
@@ -429,9 +430,9 @@ def add_patient_to_database(pat_id, att_name, pat_age):
 def get_patient_from_database(id_no):
     patlist = [x for x in patient_database if x["id"] == id_no]
     if len(patlist) == 0:
-        return "ERROR: no patient with id {} in database".format(id_no)
+        return "ERROR: No patient with id {} in database".format(id_no)
     if len(patlist) > 1:
-        return "ERROR: patient id ({}) not unique identifier".format(id_no)
+        return "ERROR: Patient id ({}) not unique identifier".format(id_no)
     return patlist[0]
 
 
@@ -451,9 +452,9 @@ def add_attending_to_database(att_name, att_email, att_phone):
 def get_attending_from_database(attendant_name):
     attlist = [x for x in attending_database if x["name"] == attendant_name]
     if len(attlist) == 0:
-        return "ERROR: no attending in database"
+        return "ERROR: No attending in database"
     if len(attlist) > 1:
-        return "ERROR: name not unique identifier"
+        return "ERROR: Name not unique identifier"
     return attlist[0]
 
 
@@ -472,6 +473,7 @@ def add_heart_rate(patient, heart_rate):
 
 
 def get_last_heart_rate(patient):
+    
     HR_data = patient["HR_data"]
     if len(HR_data) == 0:
         return None
@@ -480,6 +482,8 @@ def get_last_heart_rate(patient):
 
 def is_tachycardic(hr, age):
     """Evaluates if posted heart rate is tachycardic
+    
+    Method curated by Braden Garrison
 
     Tachycardia is defined as a heart rate that is above normal resting
     rate. Specific tachycardic values are dependent upon patient age. More
@@ -528,6 +532,8 @@ def is_tachycardic(hr, age):
 
 def tach_warning(patient, hr):
     """Creates log entry upon server receiving tachycardic HR post
+    
+    Method curated by Braden Garrison
 
     Tachycardic heart rate values are defined in the is_tachycardic
     function. Any heart rate values exceeding the specified range for
@@ -602,7 +608,7 @@ def prev_heart_rate(patient):
         list: all heart rate values stored in specified patient database
     """
     if len(patient["HR_data"]) == 0:
-        return "ERROR: no heart rate values saved for patient"
+        return "ERROR: No heart rate values saved for patient"
     else:
         hr_list = []
         for x in patient["HR_data"]:
@@ -612,6 +618,8 @@ def prev_heart_rate(patient):
 
 def heart_rate_average(hr_list):
     """Averages posted heart rates of a patient
+    
+    Method curated by Braden Garrison
 
     The accepted heart rate list of a specific patient is iterated
     through to produce a total which is then divided by the list
@@ -634,6 +642,8 @@ def heart_rate_average(hr_list):
 
 def heart_rate_interval(interval_time, patient):
     """Gives list of heart rates posted after a specified time
+    
+    Method curated by Braden Garrison
 
     Iterates through patient heart rate values to determine which
     heart rates were posted after the specified interval time,
@@ -650,7 +660,7 @@ def heart_rate_interval(interval_time, patient):
     interval_dt = dt.strptime(interval_time, "%Y-%m-%d %H:%M:%S")
     hr_interval = []
     if len(patient["HR_data"]) == 0:
-        return "ERROR: no heart rate values saved for patient"
+        return "ERROR: No heart rate values saved for patient"
     else:
         for x in patient["HR_data"]:
             hr_dt = dt.strptime(x["timestamp"], "%Y-%m-%d %H:%M:%S")
@@ -663,6 +673,8 @@ def str_to_int(value):
     """Converts an input string
     into int value, or returns input
     if input is already int
+    
+    Method curated by Anuj Som
 
     Args:
         value (int, str): Accepts an int or string to convert to int
